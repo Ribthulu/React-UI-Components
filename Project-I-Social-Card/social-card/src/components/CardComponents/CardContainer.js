2b1dcf46ecc8076d012f33e37b804d5d89696aca
Ribthulu <myemail@example.com>
2019-06-01 09:10:29 -0400

    completed html/jsx/react portion of project 1; need to still add css to style it correctly

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,19 @@
+/*jshint esversion: 6 */
+
 import React from 'react';
 import './Card.css';
+import CardBanner from "./CardBanner.js";
+import CardContent from "./CardContent.js";
+
+const CardContainer = () => {
+  return (
+    <div class="CardContainer">
+      <CardBanner />
+      <h1>Get Started with React</h1>
+      <p>React makes it painless to creat interavice UIs. Design simple views for each starte in your application</p>
+      <CardContent />
+    </div>
+  );
+};
+
+export default CardContainer;
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,12 @@
+/*jshint esversion: 6 */
+
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+  return (
+    <a href="https://reactjs.org/">Reactjs.org</a>
+  );
+};
+
+export default CardContent;
